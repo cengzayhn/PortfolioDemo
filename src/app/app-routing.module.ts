799a0798edb24a0add9c2a0d087ab3f09f878f62
cengzayhn <mcengzayhn@gmail.com>
2023-04-12 00:41:48 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { FirstPageComponent } from './components/first-page/first-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestPageComponent } from './components/test-page/test-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
+import { ThirdPageComponent } from './components/third-page/third-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:TestPageComponent,children:[
+    {path:'page-1',component:FirstPageComponent},
+    {path:'page-2',component:SecondPageComponent},
+    {path:'page-3',component:ThirdPageComponent}
+
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestPageComponent } from './components/test-page/test-page.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
+import { ThirdPageComponent } from './components/third-page/third-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule,
